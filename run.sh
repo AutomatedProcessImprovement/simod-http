5022dee2de489c3bc4a3ef5fa73f00d71a9b8042
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-02-22 15:32:13 +0200

    Dockerfile and CI workflow added

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-Xvfb :99 &>/dev/null & disown
-
-cd /usr/src/Simod/src/simod_http
-export VIRTUAL_ENV=$(poetry env info --path)
-poetry run uvicorn simod_http.main:app --host 0.0.0.0 --port 80
+poetry run uvicorn simod_http.main:api --host $SIMOD_HTTP_HOST --port $SIMOD_HTTP_PORT
