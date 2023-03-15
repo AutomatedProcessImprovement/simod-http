df3a543004e53c04a2634f96d6fada564c2ca2ae
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-03-15 19:38:34 +0200

    Bash scripts updated

---
+++ docker_build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t nokal/simod-http:$(poetry version -s) .
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-poetry run uvicorn simod_http.main:api --host $SIMOD_HTTP_HOST --port $SIMOD_HTTP_PORT
+poetry run uvicorn simod_http.main:api --host $SIMOD_HTTP_HOST --port $SIMOD_HTTP_PORT --log-level $SIMOD_HTTP_LOG_LEVEL
