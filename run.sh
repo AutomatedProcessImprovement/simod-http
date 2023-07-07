510ba21a688aca30254d543d351459f93eb1d0d9
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-07-07 15:49:28 +0300

    Update Docker scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 env
-poetry run gunicorn simod_http.main:api -k uvicorn.workers.UvicornWorker -w $SIMOD_GUNICORN_WORKERS -b $SIMOD_HTTP_ADDRESS --log-level $SIMOD_HTTP_LOG_LEVEL
+poetry run gunicorn simod_http.main:api -k uvicorn.workers.UvicornWorker -w $SIMOD_GUNICORN_WORKERS -b $SIMOD_HTTP_ADDRESS --log-level $SIMOD_LOGGING_LEVEL
